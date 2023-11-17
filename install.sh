f21ad5c4a3fadafcfea3bf62e9cb20d28be9ad41
Tasia Donnelly <1733713379@qq.com>
2023-11-17 17:11:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+function home() {
+clear
+echo
+echo "
+==================================================================
+		linux lxc 旁路 | 一键搭建脚本
+                 Powered by www.herozmy.com 2023
 
 
-function update() {
-apt install git wget nano curl -y
+     温馨提示：
+         本脚本仅在lxc debian10/11环境下安装，其他环境未测试，仅为自己所用 
+         本脚本仅适用于学习与研究等个人用途,请勿用于任何违法国家法律的活动！
 
+==================================================================
 
-
-return 1
+回车Enter继续~
+"
+read
+sleep 1
+install
 }
+
+
+
+
 function install() {
 host="https://github.com/MetaCubeX/Clash.Meta/releases/download";
 version="v1.16.0";
@@ -38,32 +54,39 @@ wget ${host}/${version}/${clash}.gz
 sleep 1
 echo "开始解压"
 gunzip ${clash}.gz >/dev/null 2>&1
+sleep 1
 echo "开始重命名"
 mv ${clash} clash >/dev/null 2>&1
+sleep 1
 echo "开始添加执行权限"
 chmod u+x clash >/dev/null 2>&1
+sleep 1
 echo "复制 clash 到 /usr/bin"
 cp clash /usr/bin >/dev/null 2>&1
+sleep 1
 echo "创建 /etc/clash 目录"
 mkdir /etc/clash >/dev/null 2>&1
 cd /etc/clash
+sleep 1
 git init 
 git remote add -f origin https://github.com/52shell/herozmy-private.git
 git config core.sparsecheckout true
 echo 'clash' >> .git/info/sparse-checkout
 git pull origin main
 cd /etc/clash/clash
+sleep 1
 sed -i "s|^external-controller: :.*|external-controller: :$uiport|" /etc/clash/clash/config.yaml
 sed -i "s|^subscribe-url:.*|subscribe-url: $suburl|" /etc/clash/clash/config.yaml
 sed -i "s|url=机场订阅|url=$suburl|" /etc/clash/clash/config.yaml
 
+sleep 1
 echo "创建ip转发"
 echo 'net.ipv4.ip_forward=1'>>/etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1'>>/etc/sysctl.conf
 #echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
 #echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
 echo "ip转发创建完成"
-
+sleep 1
 echo "开始创建 systemd 服务"
 
 cat << EOF > /etc/systemd/system/clash.service
@@ -84,6 +107,4 @@ echo "systemd 服务创建完成"
 return 1
 }
 
-
-update
-install
+home
