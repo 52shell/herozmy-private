a72c7e98a61b2b93edd33bec6ac35cf35cb3cfe4
Tasia Donnelly <1733713379@qq.com>
2024-03-24 16:53:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,8 +38,7 @@ chown -R www:www /data/wwwroot/default/
 
 #下载xdir
 wget -O /tmp/xdir.zip https://github.com/52shell/herozmy-private/releases/download/v1.4.0-xdir-up/Xdir.zip
-unzip -o /tmp/xdir.zip -d /root/
-cp -r /root/Xdir/* /data/wwwroot/default/
+
 
 #替换路径
 #sed -i "s%\"thedir.*%\"thedir\"=>'/data/wwwroot/default',%g" zdir/config.php
